fda03cd39ab76ebd77f975ea804e4527eed86926
AswathKiruba <AswathKiruba@users.noreply.github.com>
2017-08-13 17:25:37 -0400

    Shapes with Turtles
    A python programs to draw shapes with turtle library

---
+++ Turtle.py
@@ -0,0 +1,25 @@
+import turtle
+
+def draw_square(some):
+
+    for i in range(1,5):    
+        some.forward(100)
+        some.right(90)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.speed(2)
+
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+    
+    
+    window.exitonclick()
+
+    
+draw_art()
